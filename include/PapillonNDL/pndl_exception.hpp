b02eab4cb2ae335bcfd14db3be8d70bb805ddebe
Hunter Belanger <hunter.belanger@gmail.com>
2021-01-13 22:01:29 +0100

    Adds PNDLException class

---
+++ include/PapillonNDL/pndl_exception.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2021, Hunter Belanger
+ *
+ * hunter.belanger@gmail.com
+ *
+ * Ce logiciel est régi par la licence CeCILL soumise au droit français et
+ * respectant les principes de diffusion des logiciels libres. Vous pouvez
+ * utiliser, modifier et/ou redistribuer ce programme sous les conditions
+ * de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA
+ * sur le site "http://www.cecill.info".
+ *
+ * En contrepartie de l'accessibilité au code source et des droits de copie,
+ * de modification et de redistribution accordés par cette licence, il n'est
+ * offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+ * seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+ * titulaire des droits patrimoniaux et les concédants successifs.
+ *
+ * A cet égard  l'attention de l'utilisateur est attirée sur les risques
+ * associés au chargement,  à l'utilisation,  à la modification et/ou au
+ * développement et à la reproduction du logiciel par l'utilisateur étant
+ * donné sa spécificité de logiciel libre, qui peut le rendre complexe à
+ * manipuler et qui le réserve donc à des développeurs et des professionnels
+ * avertis possédant  des  connaissances  informatiques approfondies.  Les
+ * utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+ * logiciel à leurs besoins dans des conditions permettant d'assurer la
+ * sécurité de leurs systèmes et ou de leurs données et, plus généralement,
+ * à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.
+ *
+ * Le fait que vous puissiez accéder à cet en-tête signifie que vous avez
+ * pris connaissance de la licence CeCILL, et que vous en avez accepté les
+ * termes.
+ *
+ * */
+#ifndef PAPILLON_NDL_EXCEPTION_H
+#define PAPILLON_NDL_EXCEPTION_H
+
+#include <string>
+#include <exception>
+#include <stdexcept> // TO BE REMOVED ONCE EVERYTHING USES PNDLException
+
+namespace pndl {
+
+  class PNDLException : public std::exception {
+    public:
+      PNDLException(): message("\n") {}
+      PNDLException(const std::string& mssg, const std::string& file, int line): message("\n") {
+        add_to_error_message(mssg, file, line); 
+      }
+      ~PNDLException() = default;
+
+      void add_to_exception(const std::string& mssg, const std::string& file, int line) {
+        add_to_error_message(mssg, file, line);
+      }
+
+      const char* what() const noexcept override {
+        return message.c_str();
+      }
+
+    private:
+      std::string message;
+
+      void add_to_error_message(const std::string& mssg, const std::string& file, int line) {
+        std::string tmp = "\n";
+        tmp += " #--------------------------------------------------------------------------------\n";
+        tmp += " # Location: " + file + ":" + std::to_string(line) + "\n";
+        tmp += " # \n";
+        tmp += " # Message: ";
+        
+        for(const auto& c : mssg) {
+          if(c == '\n') {
+            tmp += "\n #          ";
+          } else {
+            tmp += c;
+          }
+        }
+        tmp += "\n"; 
+        tmp += " #--------------------------------------------------------------------------------";
+
+        message = tmp + message;
+      }
+  };
+}
+
+#endif
