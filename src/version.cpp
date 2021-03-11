9e4d60cbfb78d13173c6b4511984b18e427e7afa
Hunter Belanger <hunter.belanger@gmail.com>
2021-03-11 19:21:55 +0100

    Set new version info for dev branch

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@
 namespace pndl {
 const unsigned int VERSION_MAJOR = 0;
 const unsigned int VERSION_MINOR = 2;
-const unsigned int VERSION_PATCH = 0;
-const bool VERSION_DEVELOPMENT = false;
-const char* VERSION_STRING = "0.2.0";
+const unsigned int VERSION_PATCH = 1;
+const bool VERSION_DEVELOPMENT = true;
+const char* VERSION_STRING = "0.2.1-dev";
 }  // namespace pndl
