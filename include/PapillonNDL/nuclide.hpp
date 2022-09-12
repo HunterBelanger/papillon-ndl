c78d331755019132e196dee669ef91e44603ddf7
Hunter Belanger <hunter.belanger@gmail.com>
2022-09-12 20:10:43 +0200

    Fixes bug when reading nuclide from symbol with isotope ending in m.

--- include/PapillonNDL/nuclide.hpp
+++ include/PapillonNDL/nuclide.hpp
@@ -155,8 +155,9 @@ class Nuclide {
       throw err;
     }
 
+    std::string symbol_for_isomer = std::regex_replace(symbol, isotope_regex, "");
     const std::regex isomer_regex("([m][0-2])");
-    std::regex_search(symbol, match, isomer_regex);
+    std::regex_search(symbol_for_isomer, match, isomer_regex);
     std::string isomer_str(match[0].first, match[0].second);
     level_ = 0;
     if (isomer_str.size() > 0) {
