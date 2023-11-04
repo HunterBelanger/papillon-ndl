cd9b50dc4c5c36eb8db5ca2c2aff096405c2b3c0
Hunter Belanger <hunter.belanger@gmail.com>
2023-11-04 18:45:37 -0400

    Changes to docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -64,11 +64,9 @@ highlight_language = 'c++'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_theme_options = {
     'canonical_url': '',
